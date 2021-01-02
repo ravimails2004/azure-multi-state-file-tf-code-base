681e77ee89d455379f1827ff2fb48a3c8be7ac63
Ravi.Srivastav <ravi.srivastav@embibe.com>
2021-01-02 16:41:09 +0000

    Adding execute-tf.sh

--- execute-tf.sh
+++ execute-tf.sh
@@ -6,8 +6,9 @@ STRUCT="src/terraform"
 SOURCE_LN="modules"
 
 function SetupDirStructure() {
-         ${MD} -p "${JOB_NAME}/${STRUCT}" 
-         ${LINK} -s ${SOURCE_LN} "${JOB_NAME}/${STRUCT}/modules" 
+	 pushd $JOB_NAME
+         ${MD} -p "{STRUCT}" 
+         ${LINK} -s ${SOURCE_LN} "${STRUCT}/modules" 
 }
 
 function Tf_Init() {
