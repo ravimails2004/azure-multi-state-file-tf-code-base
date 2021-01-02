5ef71e910135d31c27c8e41b05ac2b79c238dead
Ravi.Srivastav <ravi.srivastav@embibe.com>
2021-01-02 15:22:18 +0000

    Adding updated code for modular approach

--- modules/resource-group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "resource-group-name" {
-  value = data.azurerm_resource_group.resource-group.name
-}
-
-output "resource-group-location" {
-  value = data.azurerm_resource_group.resource-group.location
-}
